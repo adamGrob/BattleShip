81e50324b982c8a670487ffaeeec493fb11abd87
adamGrob <ad4mgr0b@gmail.com>
2018-11-14 13:36:46 +0100

    Implemented the ship class, created a hitChecker method, it sets the ship status destroyed if all of its segments are destroyed.

--- src/main/java/com/trimmaloth/battleShip/model/Ship.java
+++ src/main/java/com/trimmaloth/battleShip/model/Ship.java
@@ -2,16 +2,20 @@ package com.trimmaloth.battleShip.model;
 
 public class Ship {
 
-    private ShipClass shipClass;
-
     private Shipsegment[] shipsegments;
 
-    public Ship(ShipClass shipClass, Shipsegment[] shipsegments) {
-        this.shipClass = shipClass;
+    private boolean destroyed;
+
+    public Ship(Shipsegment[] shipsegments) {
         this.shipsegments = shipsegments;
     }
 
-    public void hitCheker (Position position) {
 
+    public void hitChecker() {
+        int hitpoints = shipsegments.length;
+        for (Shipsegment shipsegment: shipsegments) {
+            if (shipsegment.isDestroyed()) hitpoints--;
+        }
+        if (hitpoints <= 0) destroyed = true;
     }
 }
