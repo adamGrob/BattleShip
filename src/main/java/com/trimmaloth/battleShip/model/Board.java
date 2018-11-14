68a0b10c332ba04795b30dede4026c5654246d35
adamGrob <ad4mgr0b@gmail.com>
2018-11-14 14:13:02 +0100

    Created the Board class.

---
+++ src/main/java/com/trimmaloth/battleShip/model/Board.java
@@ -0,0 +1,13 @@
+package com.trimmaloth.battleShip.model;
+
+public class Board {
+
+    int width, height;
+
+    public Board(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+
+}
